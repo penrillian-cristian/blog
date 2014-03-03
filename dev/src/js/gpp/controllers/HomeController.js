c8e22a37534e55cee578f35f2060c52c47ebc7a5
penrillian-cristian <cristian@penrillian.com>
2014-03-03 14:10:52 +0000

    fixed login

--- dev/src/js/gpp/controllers/HomeController.js
+++ dev/src/js/gpp/controllers/HomeController.js
@@ -1,26 +1,40 @@
 "use strict";
 
-ApplicationControllers.controller("HomeController", ["$scope", function($scope) {
-    $scope.posts =[
-            {id:1,title:"Sample Post Title 1", date:"27-02-2014",author:"Filip Machinia",category:"technology",content:"This is the first blog post"},
-            {id:2,title:"Sample Post Title 2", date:"27-02-2014",author:"Cristian Ivascu",category:"technology",content:"This is the second blog post"},
-            {id:3,title:"Sample Post Title 3", date:"27-02-2014",author:"Chris Allison",category:"technology",content:"This is the third blog post"}
-        ];
-        //return posts;
+ApplicationControllers.controller("HomeController", ["$scope", function ($scope) {
+    $scope.posts = [
+        {id: 1, title: "Sample Post Title 1", date: "27-02-2014", author: "Filip Machinia", category: "technology", content: "This is the first blog post"},
+        {id: 2, title: "Sample Post Title 2", date: "27-02-2014", author: "Cristian Ivascu", category: "technology", content: "This is the second blog post"},
+        {id: 3, title: "Sample Post Title 3", date: "27-02-2014", author: "Chris Allison", category: "technology", content: "This is the third blog post"}
+    ];
+    //return posts;
 
     $scope.loginDetails = [];
-    $scope.register = function(username, password){
-        $scope.loginDetails.push({username: username, password:password});
+    $scope.register = function (username, password) {
+        $scope.loginDetails.push({username: username, password: password});
     };
 
-    $scope.login = function(user, pass){
+    $scope.login = function (user, pass) {
         $scope.result = "";
-        for(var i=0; i<$scope.loginDetails.length;i++){
-            if((user ===  $scope.loginDetails[i].username)&& (pass === $scope.loginDetails[i].password) ){
-                $scope.result = "Login was successful";
-                break;
-            }else{
+
+        if (!user || !pass) {
+            $scope.result = "Required fields are empty";
+
+        } else {
+
+            if ($scope.loginDetails.length===0) {
                 $scope.result = "Invalid username or password";
+            }
+
+            else {
+                for (var i = 0; i < $scope.loginDetails.length; i++) {
+
+                    if ((user === $scope.loginDetails[i].username) && (pass === $scope.loginDetails[i].password)) {
+                        $scope.result = "Login was successful";
+                        break;
+                    } else {
+                        $scope.result = "Invalid username or password";
+                    }
+                }
             }
         }
     };
@@ -28,13 +42,13 @@ ApplicationControllers.controller("HomeController", ["$scope", function($scope) 
 }]);
 
 /*
-ApplicationControllers.controller("LoginController", ["$scope", function($scope) {
+ ApplicationControllers.controller("LoginController", ["$scope", function($scope) {
 
-    $scope.login = function(username, password){
-      // $scope.username = username;
-      // $scope.password = password;
-    };
-    //return posts;
+ $scope.login = function(username, password){
+ // $scope.username = username;
+ // $scope.password = password;
+ };
+ //return posts;
 
 
-}]);*/
+ }]);*/
